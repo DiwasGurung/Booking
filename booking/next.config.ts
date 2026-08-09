7ea6c2aa10cdd7392c3e3c426ac8c170b106da89
DiwasGurung <diwasgrg14@gmail.com>
2026-08-09 16:34:15 +0545

    frontend host

--- booking/next.config.ts
+++ booking/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'export', // Enforce static export for cPanel hosting
+  images: {
+    unoptimized: true, 
+    // Prevents image optimization errors on static exports
+  },
+   distDir: 'dist-front',
   async headers() {
     return [
       {
