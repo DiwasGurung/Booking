447fbebeed5209c38e2cc55f0d570f903476a8da
DiwasGurung <diwasgrg14@gmail.com>
2026-08-11 00:36:40 +0545

    deployment changes

--- booking/next.config.ts
+++ booking/next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  distDir: 'dist-front',
   images: {
     unoptimized: true, 
 
   },
+
   async headers() {
     return [
       {
