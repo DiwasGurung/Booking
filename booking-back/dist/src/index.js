1d8ffb92e000689ca4330315cb841af50262b144
DiwasGurung <diwasgrg14@gmail.com>
2026-08-24 01:14:43 +0545

    mb extended

--- booking-back/dist/src/index.js
+++ booking-back/dist/src/index.js
@@ -43,7 +43,8 @@ app.use((0, cors_1.default)({
     optionsSuccessStatus: 204
 }));
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.json());
+app.use(express_1.default.json({ limit: "5mb" }));
+app.use(express_1.default.urlencoded({ limit: "5mb", extended: true }));
 app.get("/", (req, res) => {
     res.send("Backend is running!");
 });
--- booking-back/src/index.ts
+++ booking-back/src/index.ts
@@ -49,7 +49,8 @@ app.use(
 
 app.use(cookieParser());
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }))
+app.use(express.urlencoded({ limit: "5mb", extended: true }))
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
